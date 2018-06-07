d6a1698aea7ad4273d6616e98a8f7316d7faec86
John F Gomez <jgomez818@gmail.com>
2018-06-06 21:33:47 -0400

    applying camera transformations

--- Galileo/app/src/main/java/john/lighterletter/com/galileo/opengl/MyGLRenderer.java
+++ Galileo/app/src/main/java/john/lighterletter/com/galileo/opengl/MyGLRenderer.java
@@ -2,11 +2,18 @@ package john.lighterletter.com.galileo.opengl;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    // creating a projection and camera view and applying it to shapes drawn in the GLSurfaceView.
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
 
     private Triangle triangle;
     private Square square;
@@ -25,11 +32,29 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
-        triangle.draw();
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Draw shape
+        triangle.draw(mMVPMatrix);
     }
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        /**
+         * Just applying a projection transformation to your drawing objects typically results in a
+         * very empty display. In general, you must also apply a camera view transformation in
+         * order for anything to show up on screen.
+         */
     }
 
 
